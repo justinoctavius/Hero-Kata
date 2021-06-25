b4d523e53d379abd6004d52ed51f7a63c82a3c0e
octavio <justinoctavio2001@gmail.com>
2021-06-24 23:13:01 -0400

    fix: Adding superpowers to heros data model

--- src/models/heros.model.tsx
+++ src/models/heros.model.tsx
@@ -8,7 +8,11 @@ export default [
       'Batman was originally introduced as a ruthless vigilante who frequently killed or maimed criminals, but evolved into a character with a stringent moral code and strong sense of justice. Unlike most superheroes, Batman does not possess any superpowers, instead relying on his intellect, fighting skills, and wealth.',
     gender: 'masculine',
     image: 'https://wallpaperstock.net/wallpapers/thumbs1/33623hd.jpg',
-    superpowers: []
+    superpowers: [
+      'Genius intellect',
+      'Expert detective',
+      'Skilled martial artist '
+    ]
   },
   {
     name: 'Diana Prince',
@@ -18,7 +22,15 @@ export default [
     description: `The "Diana Prince" identity has been part of Wonder Woman's history since her comics debut in 1941. In the early Golden Age stories, Wonder Woman served as a military secretary during World War II, using Prince as her cover. Later occupations Wonder Woman performed as Prince included translator at the United Nations, Air Force captain and ambassador, and in the '70s TV series, Lynda Carter's Wonder Woman used Prince to serve as an agent of the Inter-Agency Defense Command`,
     gender: 'woman',
     image: 'https://wallpaperstock.net/wallpapers/thumbs1/33623hd.jpg',
-    superpowers: []
+    superpowers: [
+      'Superhuman strength',
+      'Regeneration',
+      'Superhuman Speed',
+      'Superhuman reflexes',
+      'Extended longevity',
+      'Divine empowerment',
+      'Master hand-to-hand combat'
+    ]
   },
   {
     name: 'Barry Allen',
@@ -29,7 +41,16 @@ export default [
     gender: 'masculine',
     image:
       'https://www.xtrafondos.com/wallpapers/ezra-miller-como-flash-5910.jpg',
-    superpowers: []
+    superpowers: [
+      'Superhuman speed',
+      'Superhuman reflexes',
+      'Superhuman agility',
+      'Superhuman strength',
+      'Intangibility',
+      'Invisibility',
+      "Time travel",
+      "Dimension travel"
+    ]
   },
   {
     name: 'Victor "Vic" Stone',
@@ -39,7 +60,15 @@ export default [
     description: `Victor Stone is the son of Silas Stone and Elinore Stone, scientists who use him as a test subject for various intelligence enhancement projects. While these treatments are ultimately successful and Victor's IQ subsequently grows to genius levels, he grows to resent his treatment. `,
     gender: 'masculine',
     image: 'https://wallpapercave.com/wp/wp3538563.jpg',
-    superpowers: []
+    superpowers: [
+      "Superhuman strength",
+      "Superhuman speed",
+      "Superhuman durability",
+      "Advanced sensory",
+      "Data Manipulation",
+      "Self-regenerative systems",
+      "Technopathy"
+    ]
   },
   {
     name: 'Clark Joseph Kent',
@@ -51,7 +80,16 @@ export default [
     gender: 'masculine',
     image:
       'https://nicolasramospintado.files.wordpress.com/2007/02/henry-cavill-superman-wallpaper-wallpaper-4.jpg',
-    superpowers: []
+    superpowers: [
+      "powers of flight",
+      "superhuman strength",
+      " x-ray vision",
+      "heat vision",
+      "cold breath",
+      "super-speed",
+      "enhanced hearing",
+      "nigh-invulnerability."
+    ]
   },
   {
     name: 'Arthur Curry',
@@ -62,6 +100,14 @@ export default [
       'Arthur Curry is Aquaman, an adventurer who is half-human and half-Atlantean. This gives him a variety of powers including superhuman strength, telepathic control over marine life, and the ability to survive both in water and on land. He belongs to the royal family of Atlantis, and normally acts as their king.',
     gender: 'masculine',
     image: 'https://wallpaperaccess.com/full/2772776.jpg',
-    superpowers: []
+    superpowers: [
+      "Superhuman strength",
+      "Superhuman durability",
+      "Superhuman speed",
+      "Superhuman reflexes",
+      "Senses Telepathic control of all aquatic life",
+      "Master tactician Skilled hand-to-hand",
+      "Martial artist Underwater",
+    ]
   }
 ];
