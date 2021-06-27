ab2175605db5ec056a08c5706fa4ecc19b07e946
octavio <justinoctavio2001@gmail.com>
2021-06-27 14:23:19 -0400

    fix: changing the hero model

--- src/models/heros.model.tsx
+++ src/models/heros.model.tsx
@@ -1,119 +1,46 @@
-export default [
+import { Hero, Status, Type } from '../types';
+import Timeline from './../types/Timeline';
+
+const data: Hero[] = [
   {
-    id: 1,
+    id: '1',
     name: 'Bruce Wayne',
-    nickname: 'Batman',
-    raze: '	Human',
-    age: 55,
-    description:
-      'Batman was originally introduced as a ruthless vigilante who frequently killed or maimed criminals, but evolved into a character with a stringent moral code and strong sense of justice. Unlike most superheroes, Batman does not possess any superpowers, instead relying on his intellect, fighting skills, and wealth.',
-    gender: 'masculine',
-    image: 'https://wallpaperstock.net/wallpapers/thumbs1/33623hd.jpg',
-    superpowers: [
-      'Genius intellect',
-      'Expert detective',
-      'Skilled martial artist '
-    ]
+    photoUrls: ['https://wallpaperstock.net/wallpapers/thumbs1/33623hd.jpg'],
+    birth: '10/20/1890',
+    death: '10/10/1000',
+    status: Status.alive,
+    type: Type.dc
   },
   {
-    id: 2,
+    id: '2',
     name: 'Diana Prince',
-    nickname: 'Wonder Woman',
-    raze: 'Amazon',
-    age: 800,
-    description: `The "Diana Prince" identity has been part of Wonder Woman's history since her comics debut in 1941. In the early Golden Age stories, Wonder Woman served as a military secretary during World War II, using Prince as her cover. Later occupations Wonder Woman performed as Prince included translator at the United Nations, Air Force captain and ambassador, and in the '70s TV series, Lynda Carter's Wonder Woman used Prince to serve as an agent of the Inter-Agency Defense Command`,
-    gender: 'woman',
-    image: 'https://wallpaperstock.net/wallpapers/thumbs1/33623hd.jpg',
-    superpowers: [
-      'Superhuman strength',
-      'Regeneration',
-      'Superhuman Speed',
-      'Superhuman reflexes',
-      'Extended longevity',
-      'Divine empowerment',
-      'Master hand-to-hand combat'
-    ]
+    photoUrls: ['https://wallpaperstock.net/wallpapers/thumbs1/33623hd.jpg'],
+    birth: '10/20/1890',
+    death: '10/10/1000',
+    status: Status.alive,
+    type: Type.dc
   },
   {
-    id: 3,
+    id: '3',
     name: 'Barry Allen',
-    nickname: 'The Flash',
-    raze: 'Human',
-    age: 23,
-    description: `Barry Allen is an assistant scientist from the Criminal and Forensic Science Division of Central City Police Department. Barry had a reputation for being very slow, deliberate, and frequently late, which frustrated his fiancée, Iris West. One night, as he was preparing to leave work, a freak lightning bolt struck a nearby shelf in his lab and doused him with a cocktail of unnamed chemicals. As a result, Barry found that he could run extremely fast and had matching reflexes. `,
-    gender: 'masculine',
-    image:
-      'https://www.xtrafondos.com/wallpapers/ezra-miller-como-flash-5910.jpg',
-    superpowers: [
-      'Superhuman speed',
-      'Superhuman reflexes',
-      'Superhuman agility',
-      'Superhuman strength',
-      'Intangibility',
-      'Invisibility',
-      'Time travel',
-      'Dimension travel'
-    ]
+    photoUrls: [
+      'https://www.xtrafondos.com/wallpapers/ezra-miller-como-flash-5910.jpg'
+    ],
+    birth: '10/20/1890',
+    death: '10/10/1000',
+    status: Status.alive,
+    type: Type.dc
   },
   {
-    id: 4,
-    name: 'Victor "Vic" Stone',
-    nickname: 'Cyborg',
-    raze: 'Human Cyborgnoid',
-    age: 23,
-    description: `Victor Stone is the son of Silas Stone and Elinore Stone, scientists who use him as a test subject for various intelligence enhancement projects. While these treatments are ultimately successful and Victor's IQ subsequently grows to genius levels, he grows to resent his treatment. `,
-    gender: 'masculine',
-    image: 'https://wallpapercave.com/wp/wp3538563.jpg',
-    superpowers: [
-      'Superhuman strength',
-      'Superhuman speed',
-      'Superhuman durability',
-      'Advanced sensory',
-      'Data Manipulation',
-      'Self-regenerative systems',
-      'Technopathy'
-    ]
-  },
-  {
-    id: 5,
-    name: 'Clark Joseph Kent',
-    nickname: 'Superman',
-    raze: '	Kryptonian',
-    age: 33,
-    description:
-      'Superman was born on the planet Krypton and was given the name Kal-El at birth. As a baby, his parents sent him to Earth in a small spaceship moments before Krypton was destroyed in a natural cataclysm. His ship landed in the American countryside, near the fictional town of Smallville. He was found and adopted by farmers Jonathan and Martha Kent, who named him Clark Kent.',
-    gender: 'masculine',
-    image:
-      'https://nicolasramospintado.files.wordpress.com/2007/02/henry-cavill-superman-wallpaper-wallpaper-4.jpg',
-    superpowers: [
-      'powers of flight',
-      'superhuman strength',
-      ' x-ray vision',
-      'heat vision',
-      'cold breath',
-      'super-speed',
-      'enhanced hearing',
-      'nigh-invulnerability.'
-    ]
-  },
-  {
-    id: 6,
-    name: 'Arthur Curry',
-    nickname: 'Aquaman',
-    raze: 'Hybrid between Atlantic and Human',
-    age: 32,
-    description:
-      'Arthur Curry is Aquaman, an adventurer who is half-human and half-Atlantean. This gives him a variety of powers including superhuman strength, telepathic control over marine life, and the ability to survive both in water and on land. He belongs to the royal family of Atlantis, and normally acts as their king.',
-    gender: 'masculine',
-    image: 'https://wallpaperaccess.com/full/2772776.jpg',
-    superpowers: [
-      'Superhuman strength',
-      'Superhuman durability',
-      'Superhuman speed',
-      'Superhuman reflexes',
-      'Senses Telepathic control of all aquatic life',
-      'Master tactician Skilled hand-to-hand',
-      'Martial artist Underwater'
-    ]
+    id: '4',
+    name: 'Ironman',
+    photoUrls: ['https://wallpapercave.com/wp/wp3538563.jpg'],
+    birth: '10/20/1890',
+    death: '10/10/1000',
+    status: Status.alive,
+    type: Type.marvel,
+    timeline: Timeline.present
   }
 ];
+
+export default data;
