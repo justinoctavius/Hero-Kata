2dbd51f1fcf46ade890bbf467dc7a596c570bdca
octavio <justinoctavio2001@gmail.com>
2021-06-27 18:26:27 -0400

    feat: creating Action and State types

---
+++ src/types/Context.tsx
@@ -0,0 +1,11 @@
+export type State = {
+  loading: boolean;
+  payload: any;
+  error: string;
+};
+
+export type Action = {
+  type: string;
+  payload: any;
+  error: string;
+};
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,5 +1,6 @@
 import Hero from './Hero';
 import { ApiResponse, OperationResponse } from './QueryResponse';
+import { Action, State } from './Context';
 import Filter from './Filter';
 
-export { Hero, ApiResponse, OperationResponse, Filter };
+export { Hero, ApiResponse, OperationResponse, Filter, Action, State };
