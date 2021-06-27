7c52efb0297e1bd2cfae2e98142aee3b96e69425
octavio <justinoctavio2001@gmail.com>
2021-06-27 18:25:34 -0400

    feat: creting context constants

---
+++ src/constants/Context.tsx
@@ -0,0 +1,3 @@
+export const HERO_REQUEST = 'HERO_REQUEST';
+export const HERO_SUCCESS = 'HERO_SUCCESS';
+export const HERO_FAIL = 'HERO_FAIL';
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,5 +1,6 @@
 import Status from './Status';
 import Type from './Type';
 import Timeline from './Timeline';
+import * as Context from './Context';
 
-export { Status, Type, Timeline };
+export { Status, Type, Timeline, Context };
