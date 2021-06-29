b316f2276e1c899829622f059f9cbea4de824d14
octavio <justinoctavio2001@gmail.com>
2021-06-29 05:33:42 -0400

    Feat: creating theme constants

---
+++ src/constants/Theme.tsx
@@ -0,0 +1,21 @@
+export const spaces = {
+  s1: 10,
+  s2: 20
+};
+
+export const colors = {
+  primary: '#eee',
+  secondary: '#333',
+  black: '#000'
+};
+
+export const fonts = {
+  normal: 15,
+  title: 25
+};
+
+export const zIndex = {
+  z1: 9,
+  z2: 99,
+  z3: 999
+};
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,5 +2,6 @@ import Status from './Status';
 import Type from './Type';
 import Timeline from './Timeline';
 import * as Context from './Context';
+import * as Theme from './Theme';
 
-export { Status, Type, Timeline, Context };
+export { Status, Type, Timeline, Context, Theme };
